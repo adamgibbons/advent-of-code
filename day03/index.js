481f333033b53889ec3e23a2ce85d5f90e14e80f
Adam Gibbons <adam.d.gibbons@gmail.com>
2020-12-05 23:15:17 -0700

    refactor part 1 ahead of part 2

--- day03/index.js
+++ day03/index.js
@@ -2,14 +2,18 @@ const { readFileSync } = require('fs')
 
 const matrix = readFileSync('./data.txt', { encoding: 'utf8' }).split('\n').map(i => i.split(''))
 
-let pairs = []
-let x = 0
-let y = 0
+function countTreesInPath({ slopeX, slopeY}) {
+  let pairs = []
+  let x = 0
+  let y = 0
 
-do {
-  pairs.push({ x, y, char: matrix[y][x] })
-  x = (x + 3) % matrix[0].length
-  y += 1
-} while (!!matrix[y])
+  do {
+    pairs.push({ x, y, char: matrix[y][x] })
+    x = (x + slopeX) % matrix[0].length
+    y += slopeY
+  } while (!!matrix[y])
 
-console.log('solution 1:', pairs.filter(i => i.char === '#').length)
+  console.log('solution 1:', pairs.filter(i => i.char === '#').length)
+}
+
+countTreesInPath({ slopeX: 3, slopeY: 1 })
